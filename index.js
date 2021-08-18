cadebd5aa34a314b668d7c25c62cc246f8fe12d0
Gerren <g.ballard23@outlook.com>
2021-08-18 11:34:52 -0500

    commit tree 32ab7fe3327928a1c02307f08d0ef6568c30c05f

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+   customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+var bestCustomer;
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+   bestCustomer = 'maybe bob';
+   return bestCustomer 
+}
+
+const leastFavoriteCustomer = 'gerren';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'billy';
+    return leastFavoriteCustomer
+}
